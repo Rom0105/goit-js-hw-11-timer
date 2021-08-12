cbe5b50d7f9fe5c3503f74a7fd54bb5f0e22fd42
Rom0105 <savosko.photo@gmail.com>
2021-08-12 22:41:09 +0300

    правки

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.refs = {
+      days: document.querySelector('.value[data-value="days"]'),
+      hours: document.querySelector('.value[data-value="hours"]'),
+      mins: document.querySelector('.value[data-value="mins"]'),
+      secs: document.querySelector('.value[data-value="secs"]'),
+    };
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  setInt = setInterval(() => {
+    const nowDate = Date.now();
+    const time = this.targetDate - nowDate;
+    this.updateClock(time);
+  }, 500);
+
+  updateClock = (time) => {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    this.refs.days.textContent = `${days}`;
+    this.refs.hours.textContent = `${hours}`;
+    this.refs.mins.textContent = `${mins}`;
+    this.refs.secs.textContent = `${secs}`;
+  };
+
+  pad = (value) => {
+    return String(value).padStart(2, "0");
+  };
+}
 new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019"),
+  targetDate: new Date("August 28, 2021"),
 });
